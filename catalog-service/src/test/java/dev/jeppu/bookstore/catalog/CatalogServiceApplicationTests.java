0a968e9cde973aa1fb4cf6402f72be87a9ce98bb
Sujay Jeppu <sujayrj@gmail.com>
2024-07-07 07:24:11 +0530

    Refactored JUnit tests and added Readme

--- catalog-service/src/test/java/dev/jeppu/bookstore/catalog/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/dev/jeppu/bookstore/catalog/CatalogServiceApplicationTests.java
@@ -2,12 +2,12 @@ package dev.jeppu.bookstore.catalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import({ContainersConfig.class})
 @SpringBootTest
 class CatalogServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
